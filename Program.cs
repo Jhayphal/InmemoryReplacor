58cf921b541149565d63c40a8db492ff75c9eae3
Oleg Nazarenko <olegn@devart.com>
2022-08-10 00:01:33 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace InmemoryReplacor
+{
+  internal static class Program
+  {
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+      ReplaceMap map;
+
+      try
+      {
+        var config = File.ReadAllText("replace.conf");
+        map = ReplaceMap.FromJson(config);
+      }
+      catch (Exception e)
+      {
+        MessageBox.Show(e.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+        return;
+      }
+
+      while (true)
+      {
+        if (Clipboard.ContainsText())
+        {
+          var copy = Clipboard.GetText();
+
+          foreach (var @case in map.Cases)
+          {
+            var regex = new Regex(@case.Expression);
+
+            Match match = null;
+
+            try
+            {
+              match = regex.Match(copy);
+            }
+            catch (Exception e)
+            {
+              MessageBox.Show(e.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            if (match == null || !match.Success || match.Groups.Count < @case.ReplaceGroupId)
+              continue;
+
+            Group group = match.Groups[@case.ReplaceGroupId];
+
+            var newText = copy.Substring(0, group.Index) + @case.ReplaceTo + copy.Substring(group.Index + group.Length);
+            Clipboard.SetText(newText);
+          }
+        }
+
+        for (int i = 0; i < 50; ++i)
+        {
+          Thread.Sleep(10);
+          Application.DoEvents();
+        }
+      }
+    }
+  }
+}
---
+++ ReplaceMap.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace InmemoryReplacor
+{
+  public sealed class ReplaceMap
+  {
+    [JsonProperty("cases", Required = Required.Always)]
+    public Case[] Cases { get; set; }
+
+    public static ReplaceMap FromJson(string json) => JsonConvert.DeserializeObject<ReplaceMap>(json, InmemoryReplacor.Converter.Settings);
+  }
+
+  public sealed class Case
+  {
+    [JsonProperty("expression", Required = Required.Always)]
+    public string Expression { get; set; }
+
+    [JsonProperty("replace_group_id", Required = Required.Always)]
+    public int ReplaceGroupId { get; set; }
+
+    [JsonProperty("replace_to", Required = Required.Always)]
+    public string ReplaceTo { get; set; }
+  }
+
+  internal static class Converter
+  {
+    public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+    {
+      MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+      DateParseHandling = DateParseHandling.None,
+      Converters =
+            {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+    };
+  }
+}
